d42336725cc3eb1bd497118c707e7439a5a3da71
Tanuló <siked@kkszki.hu>
2024-09-23 11:28:19 +0200

    Cars class is created

---
+++ AutoConsole/Connection.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoConsole
+{
+    internal class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string Password;
+        public string User;
+        public string ConnectionString;
+        
+        public Connection()
+        {
+            Host = "localhost";
+            Database = "auto";
+            User = "root";
+            Password = "";
+            connection = new MySqlConnection(ConnectionString);
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+        }
+    }
+    
+}
